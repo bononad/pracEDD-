d97e15ffc28ed3f5081234fbb1996b88916caed1
bononad <bononadbe@gmail.com>
2016-02-22 23:11:25 +0100

    provant

--- repoEDD1/src/repoedd1/RepoEDD1.java
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -15,6 +15,7 @@ public class RepoEDD1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Nova prova");
        System.out.println("IDE Netbeans");
        System.out.print("Hello world");
        System.out.print("IDE Eclipse");
