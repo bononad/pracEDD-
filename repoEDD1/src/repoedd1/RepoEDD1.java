e4a27337a6ec547fddfc6f473730cded92456376
bononad <bononadbe@gmail.com>
2016-02-22 23:23:34 +0100

    Merge origin/master
    Conflicts:
    	repoEDD1/src/repoedd1/RepoEDD1.java

--- repoEDD1/src/repoedd1/RepoEDD1.java
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -15,7 +15,8 @@ public class RepoEDD1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Nova prova");
+       System.out.println("Nova branca"); 
+       System.out.println("Nova prova");
        System.out.println("IDE Netbeans");
        System.out.print("Hello world");
        System.out.print("IDE Eclipse");
